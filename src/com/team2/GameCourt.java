f8cba54e4f9cdd5ac8e19f31ea181adcc5b16d0b
Gustaf Matsson <gustafmatsson@gmail.com>
2018-06-20 16:52:42 +0200

    Added gamecourt that moves with arrows and draws a monster

---
+++ src/com/team2/GameCourt.java
@@ -0,0 +1,60 @@
+package com.team2;
+
+//by Gustaf Matsson
+//2018-06-20
+
+import com.googlecode.lanterna.*;
+import com.googlecode.lanterna.input.Key;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.nio.charset.Charset;
+
+public class GameCourt {
+    Terminal terminal = TerminalFacade.createTerminal(System.in,
+            System.out, Charset.forName("UTF8"));
+        terminal.enterPrivateMode();
+        terminal.setCursorVisible(false);
+    int x = 0;
+    int y = 0;
+    int x2 = 5;
+    int y2 = 5;
+    while(true) {
+//Wait for a key to be pressed
+        Key key;
+        do {
+            Thread.sleep(5);
+            key = terminal.readInput();
+        }
+        while (key == null);
+        switch (key.getKind()) {
+            case ArrowDown:
+                terminal.clearScreen();
+                terminal.moveCursor(x, ++y);
+                terminal.putCharacter('\u263A');
+                terminal.moveCursor(x2, y2 -= 2);
+                terminal.putCharacter('X');
+                break;
+            case ArrowUp:
+                terminal.clearScreen();
+                terminal.moveCursor(x, --y);
+                terminal.putCharacter('\u263A');
+                terminal.moveCursor(x2, y2 -= 2);
+                terminal.putCharacter('X');
+                break;
+            case ArrowLeft:
+                terminal.clearScreen();
+                terminal.moveCursor(--x, y);
+                terminal.putCharacter('\u263A');
+                terminal.moveCursor(x2 -= 2, y2);
+                terminal.putCharacter('X');
+                break;
+            case ArrowRight:
+                terminal.clearScreen();
+                terminal.moveCursor(++x, y);
+                terminal.putCharacter('\u263A');
+                terminal.moveCursor(x2 += 2, y2);
+                terminal.putCharacter('X');
+                break;
+        }
+    }
+}
